26c28a17f161cc36917dc7ad8272f8318895b909
Kamil128 <21.kamil.pabijan@gmail.com>
2021-06-23 11:15:54 +0200

    first model

--- src/data/preproces_dataset.py
+++ src/data/preproces_dataset.py
@@ -1,18 +1,34 @@
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
-from sklearn.utils.multiclass import unique_labels
-from sklearn.metrics import euclidean_distances
 
 import spacy
 
-nlp = spacy.load("en_core_web_lg")
+
+class TextCleanTransformer(TransformerMixin, BaseEstimator):
+    """
+
+    """
+
+    def __init__(self,
+                 nlp_model=None):
+        self.nlp_model = nlp_model
+
+    def preprocess_sentence(self, text):
+        doc = self.nlp_model(text.replace('\n', ' '))
+        output_text = []
+        for token in doc:
+            if not any([token.is_stop, token.like_url, token.is_punct,
+                        not token.is_ascii, token.like_email, token.is_space]):
+                output_text.append(token.lemma_)
+        return ' '.join(output_text)
+
+    def fit(self, X, y=None):
+        if self.nlp_model is None:
+            self.nlp_model = spacy.load("en_core_web_lg")
+        return self
+
+    def transform(self, X):
+        return np.array([self.preprocess_sentence(xi) for xi in X])
 
 
-def preprocess_sentence(text):
-    doc = nlp(text)
-    output_text = []
-    for token in doc:
-        if not any([token.is_stop, token.is_stop, token.like_url, token.is_punct]):
-            output_text.append(token.lemma_)
-    return ' '.join(output_text)
---
+++ src/visualization/plot_hisotry.py
@@ -0,0 +1,29 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+def plot_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))  # Get number of epochs
+
+    plt.plot(epochs, acc, 'r')
+    plt.plot(epochs, val_acc, 'b')
+    plt.title('Training and validation accuracy')
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.legend(["Accuracy", "Validation Accuracy"])
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'r')
+    plt.plot(epochs, val_loss, 'b')
+    plt.title('Training and validation loss')
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend(["Loss", "Validation Loss"])
+
+    plt.figure()
