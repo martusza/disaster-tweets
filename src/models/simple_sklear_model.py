35bb6446625ddc604aa0804d0aeca5a523ee9752
Kamil128 <21.kamil.pabijan@gmail.com>
2021-06-24 13:37:10 +0200

    mlflow update, add run model to makefile

--- src/models/simple_sklear_model.py
+++ src/models/simple_sklear_model.py
@@ -16,13 +16,34 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 
 def eval_metrics(actual, pred):
-    c = confusion_matrix(actual, pred)
-    a = accuracy_score(actual, pred)
-    f = f1_score(actual, pred)
-    return c, a, f
+    con_m = confusion_matrix(actual, pred)
+    accuracy = accuracy_score(actual, pred)
+    f1 = f1_score(actual, pred)
+    return con_m, accuracy, f1
 
 
 if __name__ == "__main__":
+
+    """
+    MLFlow tracking - more details:
+    https://www.mlflow.org/docs/latest/tracking.html
+    
+    Logs to directory on local machine. There is possibility to tract to server.
+    
+    Log parameters:
+    1. mlflow.log_params(dict) - log all parameters stored in dictionary
+    2. mlflow.log_param(key, value) - log each parameter separately, input key and value as string 
+    
+    Log metrics:
+    1. mlflow.log_metric(key, value) - log each metric separately, input key and value as string 
+    2. mlflow.log_metrics(dict) - log all metrics stored in dictionary
+    
+    Log model:
+    mlflow.sklearn.log_model(model, "model_name") - log model stored as .pkl file
+    
+    Autolog:
+    mlflow.sklearn.autolog()
+    """
 
     with mlflow.start_run():
         tfidf_params = {'ngram_range': (1, 1),
@@ -37,10 +58,10 @@ if __name__ == "__main__":
         y_pred = pipe.predict(X_test)
 
         cm, a, f = eval_metrics(y_test, y_pred)
-        for key in tfidf_params:
-            mlflow.log_param(key, tfidf_params[key])
 
-        # mlflow.log_metric("confusion", c)
+        mlflow.log_params(tfidf_params)
+
+        # Log metrics
         mlflow.log_metric("accuracy", a)
         mlflow.log_metric("f1", f)
 
@@ -50,4 +71,3 @@ if __name__ == "__main__":
         mlflow.log_metric("fn", f_n)
         mlflow.log_metric("tp", t_p)
 
-        mlflow.sklearn.log_model(pipe, "model")
