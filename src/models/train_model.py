024c9d2a602f1948c62f25e2a9fbe377e0e1e295
martusza <marta.warminska@gmail.com>
2021-09-11 18:20:18 +0200

    working hyperopt draft

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import joblib
 import click
-from hyperopt import hp, tpe, Trials, space_eval, fmin, STATUS_OK
+from hyperopt import hp, tpe, Trials, space_eval, fmin, STATUS_OK, space_eval
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier, LogisticRegression
@@ -18,43 +18,41 @@ np.random.seed(seed)
 
 classifiers = {
     "random_forest": RandomForestClassifier,
-    "sgd_classifier": SGDClassifier,
-    "logistic_regression": LogisticRegression,
-    "svc": SVC,
+    # "sgd_classifier": SGDClassifier,
+    # "logistic_regression": LogisticRegression,
+    # "svc": SVC,
 }
 
 hyper_space = {
     "random_forest": {
         "n_estimators": hp.choice("n_estimators", np.arange(50, 100)),
         "max_depth": hp.choice("max_depth", np.arange(10,50)),
-        "min_samples_leaf": hp.loguniform("min_samples_leaf", 0.01, 0.1),
+        "min_samples_leaf": hp.uniform("min_samples_leaf", 0.01, 0.1),
         "max_features": hp.choice("max_features", ["auto", "sqrt", "log2"]),
         "max_leaf_nodes": hp.choice("max_leaf_nodes", list(np.arange(10,30)) + [None]),
-        "n_jobs": hp.choice("n_jobs", [2]),
-        "class_weight": hp.choice("class_weight", ["balanced", "balanced_subsample"]),
+        # "n_jobs": hp.choice("n_jobs", [2]),
+        # "class_weight": hp.choice("class_weight", ["balanced", "balanced_subsample"]),
         "random_state": hp.choice("random_state", [seed])
     }
 }
 
-# define objective function
-def hyperparameter_tuning(clf, X, y):
-    search_params = hyper_space[clf]
-    acc = cross_val_score(clf(**search_params), X, y, scoring="f1").mean()
-    return {"loss": -acc, "status": STATUS_OK}
 
-
+@click.command()
 @click.option('--classifier', type=str, help="Classifier name")
 @click.option('--train-table-name', type=str, help="Training table name")
 @click.option('--test-table-name', type=str, help="Test table name")
 @click.option('--target-column', type=str, help="Target column name")
 @click.option('--output-table-name', type=str, help="Predictions test table name")
-@click.option('--num-iter', typ=int, help="Number of iterations")
+@click.option('--num-iter', type=int, help="Number of iterations", default=1)
 def main(classifier,
          train_table_name,
          test_table_name,
          target_column,
          output_table_name,
+         num_iter,
          ):
+
+    # Recognizing data type input
     data_type = Path(train_table_name).suffix
     if data_type == ".csv":
         df_train = pd.read_csv(train_table_name)
@@ -66,9 +64,49 @@ def main(classifier,
         print("Unrecognized file format")
         exit()
 
-    X_train = df_train
+    df_train.fillna(0, inplace=True)
+    y_train = df_train[target_column]
+    X_train = df_train.drop([target_column], axis=1)
 
-    cls = classifiers[classifier]
+    df_test.fillna(0, inplace=True)
+    y_test = df_test[target_column]
+    X_test = df_test.drop([target_column], axis=1)
+
+    clf = classifiers[classifier]
+    space = hyper_space[classifier]
+
+    trials = Trials()
+
+    # define objective function
+    def hyperparameter_tuning(space):
+        clf = classifiers[classifier](**space)
+        clf.fit(X_train, y_train)
+
+        # Calculating scores
+        y_train_pred = clf.predict(X_train)
+        y_test_pred = clf.predict(X_test)
+        train_score = f1_score(y_train, y_train_pred, average='macro')
+        test_score = f1_score(y_test, y_test_pred, average="macro")
+        print(
+            "Calculating f1 metric.\nTrain score: {}\nTest score: {}".format(train_score, test_score)
+        )
+        return -test_score
+
+
+    best = fmin(
+            fn=hyperparameter_tuning,
+            space=space,
+            algo=tpe.suggest,
+            max_evals=num_iter,
+            trials=trials,
+    )
+
+    print("Best: {}".format(best))
+
+    best_model = classifiers[classifier](**best)
+    best_model.fit(X_train, y_train)
+    X_test['predicted'] = best_model.predict(X_test)
+    X_test.to_csv(output_table_name)
 
 
 if __name__ == "__main__":
